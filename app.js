50db07dcf48903bd60e0b4931f290a1e059ec31e
beth nduta <bethnduta05@gmail.com>
2022-03-19 12:18:48 +0300

    modified the js file and added some media queries to make the page responsive

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ let searchbtn = document.querySelector('#search');
 let searchform = document.querySelector('.head.search-bar');
 
 searchbtn.onclick = () => {
-    searchform.classList.toggle('active')
+    searchfrm.classList.toggle('active');
+    menu.classList.remove('active');
 }
+
+let menu = document.querySelector('.head .navbar');
+menu.onclick = () => {
+    menu.classList.toggle('active');
+    searchfrm.classList.remove('active');
