a023d170cdbddec3e0ecaa081f085160cc517e86
beth nduta <bethnduta05@gmail.com>
2022-03-19 11:58:44 +0300

    modified the navbar using my css

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('hello')
+let searchbtn = document.querySelector('#search');
+let searchform = document.querySelector('.head.search-bar');
+
+searchbtn.onclick = () => {
+    searchform.classList.toggle('active')
+}
